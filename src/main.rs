58fbe19f2c978374c0a60f905a05be0a7dc0b3e5
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-12-12 22:13:04 +0000

    feat: use camera width org and forward vec

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use sphere::Sphere;
+use std::f32;
 use vec3::{Float, Vec3};
 
 use crate::canvas::Canvas;
@@ -8,20 +9,22 @@ pub mod sphere;
 pub mod vec3;
 
 fn get_ray(c: &Canvas, x: usize, y: usize) -> Vec3 {
-    let ar = c.ar;
-    let angle = c.angle;
-
-    let x = (2.0 * (x as Float) / (c.width as Float) - 1.0) * angle * ar;
-    let y = (2.0 * (y as Float) / (c.height as Float) - 1.0) * angle;
-
-    Vec3::new(x, y, -1.0)
+    let x = 2.0 * (x as Float) / (c.width as Float) - 1.0;
+    let y = 2.0 * (y as Float) / (c.height as Float) - 1.0;
+    let up = &Vec3::new(0.0, 1.0, 0.0);
+    let forword = &Vec3::new(0.0, 0.0, 1.0).norm();
+    let right = &forword.cross(up);
+    let h = (f32::consts::PI * 0.5 * 70.0 / 180.).tan();
+    let w = h * c.ar;
+    let dir = forword + &(&(up * h) * (y as Float));
+    &dir + &(&(right * w) * (x as Float))
 }
 
 fn main() {
     let mut canvas = Canvas::new(1000, 1000);
     let c1 = canvas.clone();
-    let org = Vec3::new(0.0, 0.0, -1.0);
-    let light = -&Vec3::new(-1.0, 1.0, -1.0).norm();
+    let org = Vec3::new(0.0, 0.0, 1.0);
+    let light = Vec3::new(-1.0, -1.0, -1.0).norm();
     let sphere = Sphere::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 0.0, 1.0), 0.5);
     let bg = Vec3::new(0.0, 0.0, 0.0);
     canvas.for_each(|pixel, x, y| {
@@ -33,7 +36,7 @@ fn main() {
                 let f = norm.dot(&light);
                 let color = &sphere.color * f;
                 color.apply(pixel);
-            },
+            }
             None => bg.apply(pixel),
         };
     });
