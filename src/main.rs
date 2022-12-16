8891ba9c775b6eb3746079dce493f81dc8eb460f
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-12-16 12:25:33 +0000

    chore: move trace out of clojure

--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,50 @@ pub mod scene;
 pub mod utils;
 pub mod world;
 
+fn trace(ray: &Ray, spheres: &[Sphere], lights: &[Vec3] ) -> Vec3{
+    let mut sphere: Option<&Sphere> = None;
+    let mut t = Float::INFINITY;
+    for s in spheres.iter() {
+        if let Some((t0, _t1)) = s.intersect(ray.org, ray.dir) {
+            if t0 < t {
+                sphere = Some(s);
+                t = t0;
+            }
+        };
+    }
+    let bg = Vec3::new(0.0, 0.0, 0.0);
+    match sphere {
+        Some(s) => {
+            let hitp = ray.position(t);
+            let norm = hitp.norm();
+            let f = norm.dot(&lights[0]);
+            &s.color * f
+        }
+        None => bg,
+    }
+}
+
 fn main() {
     let mut canvas = Canvas::new(1000, 1000);
     let spheres = vec![
-        Sphere::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 0.0, 1.0), 0.5),
+        Sphere::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 0.0, 1.0), 1.0),
     ];
     let camera = Camera::new(
-        Vec3::new(0.0, 0.0, 1.0),
+        Vec3::new(0.0, 0.0, 3.0),
         Vec3::new(0.0, 0.0, 1.0).norm(),
         Vec3::new(0.0, 1.0, 0.0),
         90.0,
         canvas.width,
         canvas.height,
     );
-    let light = -&Vec3::new(-0.0, 0.0, 3.0).norm();
-    let bg = Vec3::new(0.0, 0.0, 0.0);
+    let lights = vec![
+        Vec3::new(-2.0, -2.0, -2.0),
+    ];
     canvas.for_each(|pixel, x, y| {
         let dir = camera.get_ray(x, y);
         let ray = Ray::new(&camera.org, &dir);
-        let mut sphere: Option<&Sphere> = None;
-        let mut t = Float::INFINITY;
-        for s in spheres.iter() {
-            if let Some((t0, _t1)) = s.intersect(&camera.org, &dir) {
-                if t0 < t {
-                    sphere = Some(s);
-                    t = t0;
-                }
-            };
-        }
-        match sphere {
-            Some(s) => {
-                let hitp = ray.position(t);
-                let norm = hitp.norm();
-                let f = norm.dot(&light);
-                let color = &s.color * f;
-                color.apply(pixel);
-            }
-            None => bg.apply(pixel),
-        }
+        let color = trace(&ray, &spheres, &lights);
+        color.apply(pixel)
     });
     canvas.export_ppm("file.ppm").ok();
 }
