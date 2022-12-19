7a9386c2dab382672e0fe424673b955bbaeaeeef
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-12-19 10:27:14 +0000

    test: add tests for world struct

--- src/world/w.rs
+++ src/world/w.rs
@@ -1,9 +1,15 @@
 use crate::utils::{
+    material::Material,
+    matrix::Mat,
     ray::Ray,
     vec3::{Float, Vec3},
 };
 
-use super::{camera::Camera, light::Light, sphere::Sphere};
+use super::{
+    camera::{self, Camera},
+    light::Light,
+    sphere::Sphere,
+};
 
 #[derive(Debug)]
 pub struct World {
@@ -37,6 +43,37 @@ impl World {
         }
     }
 
+    pub fn default() -> Self {
+        let camera = Camera::new(1000, 1000, 45.0, Mat::identity(4));
+        let lights = vec![Light::new(
+            Vec3::new(-10.0, 10.0, -10.0),
+            Vec3::new(1.0, 1.0, 1.0),
+        )];
+        let spheres = vec![
+            Sphere::new(
+                Material {
+                    color: Vec3::new(0.8, 1.0, 0.6),
+                    diffuse: 0.7,
+                    specular: 0.2,
+                    ..Material::default()
+                },
+                Mat::identity(4),
+            ),
+            Sphere::new(
+                Material {
+                    ..Material::default()
+                },
+                Mat::identity(4).scaling(0.5, 0.5, 0.5),
+            ),
+        ];
+
+        World {
+            camera,
+            lights,
+            spheres,
+        }
+    }
+
     pub fn intersect<'a, 'b>(
         &'a self,
         ray: &Ray,
@@ -63,5 +100,25 @@ impl World {
         vec.extend(iter);
         vec.sort_by(|a, b| a.t0.partial_cmp(&b.t0).unwrap());
         vec
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_intersect() {
+        let w = World::default();
+        let r = Ray::new(Vec3::new(0.0, 0.0, -5.0), Vec3::new(0.0, 0.0, 1.0));
+        let is = w.intersect(&r, Vec::new());
+
+        assert_eq!(is.len(), 2);
+        assert_eq!(is[0].t0, 4.0);
+        assert_eq!(is[0].t1, 6.0);
+        assert_eq!(is[1].t0, 4.5);
+        assert_eq!(is[1].t1, 5.5);
     }
 }
